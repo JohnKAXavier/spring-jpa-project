24d9a4ee96f6f16c609716ea991e0e916bfaf9f4
John Xavier <johnxavier@addtech.com.br>
2021-02-18 00:42:41 -0300

    initial commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,78 @@
+package com.example.demo.student;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+@Entity
+@Table
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private LocalDate dateOfBirthday;
+
+    @Transient
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String email, LocalDate dateOfBirthday) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dateOfBirthday = dateOfBirthday;
+    }
+
+    public Student(String name, String email, LocalDate dateOfBirthday) {
+        this.name = name;
+        this.email = email;
+        this.dateOfBirthday = dateOfBirthday;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirthday() {
+        return dateOfBirthday;
+    }
+
+    public void setDateOfBirthday(LocalDate dateOfBirthday) {
+        this.dateOfBirthday = dateOfBirthday;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dateOfBirthday,LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
